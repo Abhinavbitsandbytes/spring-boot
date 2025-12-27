c9b17f790f585ae90c56570a6ab65823eff365bf
Abhinav <abhinavkr197@gmail.com>
2025-12-27 16:04:26 +0530

    1.3-beans-in-spring-framework

--- src/main/java/com/example/abhinavkr/module1introduction/Module1introductionApplication.java
+++ src/main/java/com/example/abhinavkr/module1introduction/Module1introductionApplication.java
@@ -11,13 +11,19 @@ public class Module1introductionApplication implements CommandLineRunner {
 
 @Autowired
 PaymentService paymentServiceObj;
+
+@Autowired
+PaymentService	paymentServiceObj2;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Module1introductionApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		System.out.println(paymentServiceObj.hashCode());
+		System.out.println(paymentServiceObj2.hashCode());// both hash code are same because by default beans are singleton
+// when we use @Scope("prototype"), then objects will different so will be the hashcode.
 		paymentServiceObj.pay();
-
 	}
 }
