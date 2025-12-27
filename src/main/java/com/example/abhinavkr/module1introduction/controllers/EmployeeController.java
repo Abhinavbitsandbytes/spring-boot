39ef8775463370bb84df5ea50dfc699d20cc0787
Abhinav <abhinavkr197@gmail.com>
2025-12-28 00:03:00 +0530

    2.2.2- the-presentation-layer-dto-and-controllers (set up dto and read and extracted url path)

--- src/main/java/com/example/abhinavkr/module1introduction/controllers/EmployeeController.java
+++ src/main/java/com/example/abhinavkr/module1introduction/controllers/EmployeeController.java
@@ -1,14 +1,29 @@
 package com.example.abhinavkr.module1introduction.controllers;
 
+import com.example.abhinavkr.module1introduction.dto.EmployeeDTO;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 public class EmployeeController {
-    @GetMapping(path = "/getSecretMessage")
-    public String getMySuperSecretMessage() {
-        return "secret message: 1A@secretMessage";
+
+    @GetMapping("/employee/{employeeId}") // variable name employeeId should be same here and in the EmployeeDTO argument
+    public EmployeeDTO getEmployeeById(@PathVariable Long employeeId){
+        return new EmployeeDTO(employeeId, "Abhinav", "abhinav@gmail.com", 30, LocalDate.of(2025, 8, 18), true);
+
     }
 }
-// now if we will go to http://localhost:8080/getSecretMessage
-// output will be: secret message: 1A@secretMessage
+
+// if you will go to http://localhost:8080/employee/1997
+//output ={
+//"id": 1997,
+//"name": "Abhinav",
+//"email": "abhinav@gmail.com",
+//"age": 30,
+//"dateOfJoining": "2025-08-18",
+//"active": true
+//}
+
