e586da1c16c1cce3d6c64982f45c36572e8631db
Abhinav <abhinavkr197@gmail.com>
2025-12-27 15:33:16 +0530

    initial commit

---
+++ src/main/java/com/example/abhinavkr/module1introduction/PaymentService.java
@@ -0,0 +1,11 @@
+package com.example.abhinavkr.module1introduction;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class PaymentService {
+    public void pay(){
+        System.out.println("paying...");
+    }
+}
