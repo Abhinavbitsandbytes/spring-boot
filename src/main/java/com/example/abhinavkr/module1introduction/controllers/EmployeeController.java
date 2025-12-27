12f57e7be170afec34b1b981e9c24d5e90554934
Abhinav <abhinavkr197@gmail.com>
2025-12-28 00:23:48 +0530

    2.2.3- the-presentation-layer-dto-and-controllers (read query params)

--- src/main/java/com/example/abhinavkr/module1introduction/controllers/EmployeeController.java
+++ src/main/java/com/example/abhinavkr/module1introduction/controllers/EmployeeController.java
@@ -3,27 +3,28 @@ package com.example.abhinavkr.module1introduction.controllers;
 import com.example.abhinavkr.module1introduction.dto.EmployeeDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 public class EmployeeController {
-
-    @GetMapping("/employee/{employeeId}") // variable name employeeId should be same here and in the EmployeeDTO argument
-    public EmployeeDTO getEmployeeById(@PathVariable Long employeeId){
+    @GetMapping(path = "/employee/{employeeId}")
+    // variable name employeeId should be same here and in the EmployeeDTO argument
+    public EmployeeDTO getEmployeeById(@PathVariable Long employeeId) {
         return new EmployeeDTO(employeeId, "Abhinav", "abhinav@gmail.com", 30, LocalDate.of(2025, 8, 18), true);
 
     }
+
+    @GetMapping(path = "/employees")
+    public String getAllEmployees(@RequestParam Integer age, @RequestParam String sortBy) {
+        return "Hi age " + age + " " + sortBy;
+    }
 }
 
-// if you will go to http://localhost:8080/employee/1997
-//output ={
-//"id": 1997,
-//"name": "Abhinav",
-//"email": "abhinav@gmail.com",
-//"age": 30,
-//"dateOfJoining": "2025-08-18",
-//"active": true
-//}
-
+//if url = "http://localhost:8080/employees?age=30&sortBy=price"
+// output = "Hi age 30 price"
+//if you want to make params optional
+//public String getAllEmployees(@RequestParam(required=false) Integer age, @RequestParam(required=false) String sortBy)
