c6e033d72d092f22589e0916cc872c4f946febc4
Abhinav <abhinavkr197@gmail.com>
2025-12-27 17:58:26 +0530

    1.4.4-dependency-injection-in-spring-framework (constructor DI)

--- src/main/java/com/example/abhinavkr/module1introduction/Module1introductionApplication.java
+++ src/main/java/com/example/abhinavkr/module1introduction/Module1introductionApplication.java
@@ -10,8 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Module1introductionApplication implements CommandLineRunner {
 
-	@Autowired
-	NotificationService notificationServiceObj;
+	final NotificationService notificationServiceObj;
+
+	public Module1introductionApplication(NotificationService notificationServiceObj){
+		this.notificationServiceObj = notificationServiceObj; // constructor DI (preferred instead of Field injection)
+
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(Module1introductionApplication.class, args);
 	}
@@ -21,8 +25,3 @@ public class Module1introductionApplication implements CommandLineRunner {
 		notificationServiceObj.send("hello");
 	}
 }
-// if we use @Component annotation in both EmailNotification and Sms Service we will get error -
-// Field notificationServiceObj in com.example.abhinavkr.module1introduction.Module1introductionApplication required a single bean, but 2 were found:
-
-//solution
-//1 - annotate anyone with Primary. if you add in email, output - email sending...hello
